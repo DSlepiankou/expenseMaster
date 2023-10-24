a0fe62384adf330114aae16f32f40584346c9ec1
d.slepiankou <>
2023-10-24 11:47:11 +0200

    initial commit

---
+++ ExpenseMaster/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpenseMaster
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
