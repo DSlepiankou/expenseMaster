ac2139b9ec940ca6a499aafb3fea29b3d5e32a01
d.slepiankou <>
2023-10-24 16:39:08 +0200

    added hamburger menu

---
+++ ExpenseMaster/Pages/AnotherPage.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Maui.Markup;
+using ExpenseMaster.Models;
+
+namespace ExpenseMaster.Pages
+{
+    public class AnotherPage : ContentPage
+    {
+        public AnotherPage()
+        {
+            ToolbarItems.Add(new ToolbarItem
+            {
+                IconImageSource = "add.png",
+                Text = "Elfkbnm"
+            });
+
+            Content = new StackLayout{
+               new Label()
+               .Bind(Label.TextProperty, nameof(TotalInfo.TotalSpent))
+               .Bold()
+               .Bottom()
+               .Column(1)
+               .Row(0),
+                new Label()
+               .Bind(Label.TextProperty, nameof(TotalInfo.SavedMoney))
+               .Bold()
+               .Bottom()
+               .Column(1)
+               .Row(1)
+            }.Padding(10, 10);
+        }
+    }
+}
---
+++ ExpenseMaster/Pages/MainPage.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Maui.Markup;
+using ExpenseMaster.Models;
+
+namespace ExpenseMaster.Pages
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Frame frame = new Frame
+            {
+                BorderColor = Colors.Gray,
+                CornerRadius = 10,
+                Content = new Label { Text="123"}
+            };
+        }
+    }
+}
