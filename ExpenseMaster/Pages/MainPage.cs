5d86e33deca0490c2c05ac60747bcc6c308e4694
d.slepiankou <>
2023-10-25 15:38:59 +0200

    added grid for frame

--- ExpenseMaster/Pages/MainPage.cs
+++ ExpenseMaster/Pages/MainPage.cs
@@ -1,18 +1,56 @@
 ﻿using CommunityToolkit.Maui.Markup;
 using ExpenseMaster.Models;
+using Microsoft.Maui.Controls;
 
 namespace ExpenseMaster.Pages
 {
-    public class MainPage : ContentPage
+    public partial class MainPage : ContentPage
     {
+        public class MainPageFrame : ViewCell
+        {
+            public MainPageFrame()
+            {
+                View = new Frame
+                {
+                    BorderColor = Colors.Gray,
+                    CornerRadius = 10,
+                    Content = new StackLayout
+                    {
+                        Children =
+                        {
+                            new Label()
+                            .Bind(Label.TextProperty, nameof(TotalInfo.TotalSpent), stringFormat : "Total money spent: {0}")
+                            .Bold(),
+                            new Label()
+                            .Bind(Label.TextProperty, nameof(TotalInfo.SavedMoney), stringFormat : "Total saved money: {0}")
+                            .Bold()
+                        }
+                    }
+                };
+            }
+        }
+
         public MainPage()
         {
-            Frame frame = new Frame
+            var listView = new ListView(ListViewCachingStrategy.RecycleElement);
+            listView.HasUnevenRows = true;
+            listView.SelectionMode = ListViewSelectionMode.None;
+            listView.ItemsSource = new[]
             {
-                BorderColor = Colors.Gray,
-                CornerRadius = 10,
-                Content = new Label { Text="123"}
+                new TotalInfo
+                {
+                    SavedMoney = 100,
+                    TotalSpent = 120
+                }
             };
+
+            
+
+            listView.ItemTemplate = new DataTemplate(typeof(MainPageFrame));
+            
+
+            Content = listView;
         }
+            
     }
 }
